ad8fa3ba176b992906f85e7433bd786823db8cec
Victor Molero <victor.molero@gmail.com>
2024-06-24 17:39:26 +0200

    add cors and hono http logger

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,33 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import CryptoService from './services/CryptoService.ts'
 import DatabaseService from './services/DatabaseService.ts'
+import BootstrapService from './services/BootstrapService.ts'
 
 const api = new Hono()
 const crypto = CryptoService.createCryptoService()
 const databaseService = DatabaseService.getInstance()
 
-api.get('/', async (c) => {
+const bootstrapService = new BootstrapService()
+
+await bootstrapService.bootstrap()
+
+api.use(logger())
+api.use('/api/*', cors())
+api.get('/api/health-check', async (c) => {
   try {
     const [res]: { id: number; createdAt: Date }[] = await databaseService
       .query<{ id: number; createdAt: Date }>(
         'select * from healthcheck',
       )
     const helloMessage = await crypto.encrypt(res.createdAt.toISOString())
-    return c.text(`${helloMessage} => ${await crypto.decrypt(helloMessage)}`)
+    return c.text(`Health Check OK! => ${await crypto.decrypt(helloMessage)}`)
   } catch (err) {
     return c.text(err.message)
   }
 })
+
+api.notFound((c) => c.json({ message: 'Not Found' }, 404))
 
 export default api
